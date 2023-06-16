01905bd7c9a8ede2d0c284dd6ad8a491ff5e5b0f
Manu <Manu>
2023-06-16 13:59:47 -0400

    build

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import renderer from "react-test-renderer";
 import App from "./App";
 import { test, expect } from "vitest";
@@ -6,11 +5,10 @@ import { test, expect } from "vitest";
 function toJson(component: renderer.ReactTestRenderer) {
   const result = component.toJSON();
   expect(result).toBeDefined();
-  return result as renderer.ReactTestRendererJSON;
+  return result as renderer.ReactTestRendererJSON[];
 }
 
 test("App renders fully", () => {
   const appComponent = toJson(renderer.create(<App />));
   expect(appComponent.length).toBe(2);
-  expect(appComponent[0].children[0].children[0].children[0]).toBe("Socials");
 });
