f6b8cd8054113c291ac9a5c100482d2093cdffb2
Manu Fourneau <manu@sagetap.io>
2024-08-10 18:12:46 -0400

    wedding work

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import 'react-pdf/dist/esm/Page/TextLayer.css';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Resume from './components/resume';
 import Wedding from './components/Wedding';
+import Resume from './components/Resume';
 
 function App() {
   const router = createBrowserRouter([
